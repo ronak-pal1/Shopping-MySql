df7cdd6ed7a542b95fff72cc5aea5a1c5a1a7638
Ronak Paul <ronakpaul882@gmail.com>
2023-12-22 19:41:56 +0530

    updated files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shopping Website
+
+## Express.js & MySQl
--- server.js
+++ server.js
@@ -4,6 +4,19 @@ import "dotenv/config";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.listen(3000, () => {
+  console.log("server is listening to port 3000...");
+});
+
 const database = createConnection({
   host: "localhost",
   port: "3306",
@@ -16,4 +29,22 @@ database.connect((err) => {
   else console.log("Database Connected successfully");
 });
 
-app.get("/items", (req, res) => {});
+app.get("/items", (req, res) => {
+  const category = req.query.category;
+
+  database.query("use ecommarce;", (err, response) => {
+    if (err) throw err;
+
+    console.log("database changed");
+  });
+
+  database.query(
+    `select * from items where category = '${category}';`,
+    (err, response) => {
+      if (err) throw err;
+
+      console.log("got the mysql data");
+      res.send(response);
+    }
+  );
+});
